5906d53b1bd6e59c46c037cacf30273c1d532b68
Happy2012 <yanliamanda@163.com>
2017-02-22 14:40:12 +0800

    add more exercise code

---
+++ Config.py
@@ -0,0 +1,8 @@
+from ConfigParser import ConfigParser
+CONFIGFILE = "PythonConfig.txt"
+config = ConfigParser()
+config.read(CONFIGFILE)
+print config.get('messages','greeting')
+radius = input(config.get('messages','question') + '')
+print config.get('messages', 'result_message')
+print config.getfloat('numbers', 'pi') * radius **2
